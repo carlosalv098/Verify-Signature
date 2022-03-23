b218fab1c6f984ca39ee115a97605ff520c730d4
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-03-23 11:30:30 -0600

    test

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,14 +3,37 @@ const { deployments, ethers } = require("hardhat");
 
 describe("RestrictedContract", function () {
 
-  let restrictedContract;
+  let restrictedContract, contract_address, owner, user1, user2;
 
   before(async() => {
+    [owner, user1, user2] = await ethers.getSigners();
     await deployments.fixture(["all"]);
     restrictedContract = await ethers.getContract("RestrictedContract");
+    contract_address = restrictedContract.address;
   })
 
   it("Verify signature", async function () {
-    console.log(restrictedContract.address);
+    
+    const message = ethers.utils.solidityKeccak256(
+      ['address', 'address'],
+      [contract_address, owner.address]
+    )
+    console.log(`Message is: ${message}\n`);
+
+    const arrayifyMessage = ethers.utils.arrayify(message);
+    console.log(`Arrayify message is: ${arrayifyMessage}\n`);
+
+    const flatSignature = await owner.signMessage(arrayifyMessage);
+    console.log(`Flat signature is: ${flatSignature}\n`);
+
+    // this should fail, user2 is not approved
+    let tx = await restrictedContract.connect(user2).verifyMessage(flatSignature)
+    let result = tx[1];
+    expect(result).to.be.false;
+
+    // this should pass, owner is approved
+    tx = await restrictedContract.connect(owner).verifyMessage(flatSignature)
+    result = tx[1];
+    expect(result).to.be.true;
   });
 });
